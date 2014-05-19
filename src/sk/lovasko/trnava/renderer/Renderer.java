0f7aeaab3ba7e898dac2fa4a63492ab6ab0eca0f
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-19 10:04:55 +0200

    Normalized the Renderer interface

--- src/sk/lovasko/trnava/renderer/Renderer.java
+++ src/sk/lovasko/trnava/renderer/Renderer.java
@@ -1,21 +1,27 @@
-package zapoctak;
+package sk.lovasko.trnava.renderer;
 
-/** Interface for fractal renderers
- * 
- * @author daniel
- */
-public interface fractal_renderer {
+import sk.lovasko.trnava.strategy.Strategy;
+import sk.lovasko.trnava.palette.Palette;
 
-    /** Generate fractal image using custom technology
-     * 
-     * @param minx fractal top left X coordinate
-     * @param miny fractal top left Y coordinate
-     * @param maxx fractal bottom right X coordinate
-     * @param maxy fractal bottom right Y coordinate
-     * @param limit fractal detail
-     * @param s used strategy 
-     * @param p used palette
-     * @return generated fractal image
-     */
-    java.awt.image.BufferedImage render(double minx, double miny, double maxx, double maxy, int limit, strategy s, palette p);
+import java.awt.image.BufferedImage;
+
+public interface fractal_renderer 
+{
+	/** 
+	 * Generate fractal image using custom technology.
+	 * 
+	 * @param minx fractal top left X coordinate
+	 * @param miny fractal top left Y coordinate
+	 * @param maxx fractal bottom right X coordinate
+	 * @param maxy fractal bottom right Y coordinate
+	 * @param limit fractal detail
+	 * @param s used strategy 
+	 * @param p used palette
+	 * @return generated fractal image
+	 */
+	BufferedImage 
+	render (final double minx, final double miny, final double maxx, 
+	    final double maxy, final int limit, final Strategy strategy, 
+	    final Palette palette);
 }
+
