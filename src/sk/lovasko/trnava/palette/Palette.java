edf4b38cc7f9ae4cd44cf346741aeda42e1e4b35
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-19 09:13:33 +0200

    Normalized the Palette interface

---
+++ src/sk/lovasko/trnava/palette/Palette.java
@@ -0,0 +1,13 @@
+package sk.lovasko.trnava.palette
+
+public interface Palette 
+{
+	/** 
+	 * Convert decision to pixel value depending on palette
+	 * 
+	 * @param source decision of paint strategy
+	 * @return RGB value of pixel
+	 */
+	int 
+	decide (final double source);
+}
