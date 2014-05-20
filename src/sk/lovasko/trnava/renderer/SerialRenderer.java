9827012aceff0cfc7da1650fa795a91c8bc97b21
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-20 21:29:15 +0200

    Completely reworked the SerialRenderer

--- src/sk/lovasko/trnava/renderer/SerialRenderer.java
+++ src/sk/lovasko/trnava/renderer/SerialRenderer.java
@@ -1,56 +1,31 @@
-package zapoctak;
+package sk.lovasko.trnava.renderer;
 
-import java.awt.image.BufferedImage;
+import sk.lovasko.trnava.strategy.Strategy;
+import sk.lovasko.trnava.palette.Palette;
 
-/** Serial implementation of fractal renderer
- * 
- * @author daniel
- */
-public class serial implements fractal_renderer {
+import java.awt.Point;
+import java.awt.Dimension;
 
-    /** Generate fractal image using serial technology
-     * 
-     * @param minx fractal top left X coordinate
-     * @param miny fractal top left Y coordinate
-     * @param maxx fractal bottom right X coordinate
-     * @param maxy fractal bottom right Y coordinate
-     * @param limit fractal detail
-     * @param s used strategy 
-     * @param p used palette
-     * @return generated fractal image
-     */
-    public BufferedImage render(double minx, double miny, double maxx, double maxy, int limit, strategy s, palette p) {
-        java.awt.image.BufferedImage result = new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB);
-        boolean morethan2 = false;
-        double xx, yy; 
-        double r, i;
-        int pocet;
-        
-        for (int y = 0; y < 400; y++) {
-            yy = -((double) y / 400 * (maxy - miny) + miny);
-            for (int x = 0; x < 600; x++) {
-                xx = (double) x / 600 * (maxx - minx) + minx;
-                Complex c = new Complex(xx, yy);
-                Complex z = new Complex(0.0f, 0.0f);
-                for (pocet = 0; pocet < limit; pocet++) {
-                    Complex momo = z.times(z);
-                    z = momo;
-                    
-                    momo = z.plus(c);
-                    z = momo;
-                    
-                    r = z.re();
-                    i = z.im();
-                    if (i * i + r * r > 4) {
-                        morethan2 = true;
-                        break;
-                    }
-                }
-                result.setRGB(x, y, p.decide(s.decide(x, y, morethan2, limit, pocet)));
-                morethan2 = false;
-            }
-        }
-        
-        return result;
-    }
+public class SerialRenderer implements Renderer 
+{
+	public BufferedImage 
+	render (final double minx, final double miny, final double maxx, 
+	    final double maxy, final int max_limit, final Strategy strategy, 
+	    final Palette palette, final Dimension size)
+	{
+		Worker worker = new Worker();
+		worker.minx = minx;
+		worker.miny = miny;
+		worker.maxx = maxx;
+		worker.maxy = maxy;
+		worker.max_limit = max_limit;
+		worker.strategy = strategy;
+		worker.palette = palette;
+		worker.start = new Point(0.0, 0.0);
+		worker.size = size;
+
+		Result result = worker.call();		
+		return result.image;
+	}
 }
+
