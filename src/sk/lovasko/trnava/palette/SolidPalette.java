a340b8ca838f0d34f3864ee985c2068cd78d5c37
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-22 00:29:16 +0200

    Whitespace addition

--- src/sk/lovasko/trnava/palette/SolidPalette.java
+++ src/sk/lovasko/trnava/palette/SolidPalette.java
@@ -5,7 +5,8 @@ public final class SolidPalette implements Palette
 	private final int background_color;
 	private final int foreground_color;
 
-	public SolidPalette (final int background_color, final int foreground_color)
+	public 
+	SolidPalette (final int background_color, final int foreground_color)
 	{
 		this.background_color = background_color;
 		this.foreground_color = foreground_color;
