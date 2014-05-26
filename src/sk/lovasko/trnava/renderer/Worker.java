adaba1608405611665a2d3e181c34c64d42b9f0f
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-26 15:07:19 +0200

    Reworked the worker

--- src/sk/lovasko/trnava/renderer/Worker.java
+++ src/sk/lovasko/trnava/renderer/Worker.java
@@ -1,93 +1,69 @@
-package zapoctak;
+package sk.lovasko.trnava.renderer;
+
+import sk.lovasko.trnava.strategy.Strategy;
+import sk.lovasko.trnava.palette.Palette;
+import sk.lovasko.trnava.math.Complex;
+import sk.lovasko.trnava.math.Interpolation;
 
 import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.Dimension;
+import java.util.concurrent.Callable;
 
-/** 
- * 
- * @author daniel
- */
-public class worker extends Thread {
+public final class Worker implements Callable<Result>
+{
+	public double minx; 
+	public double miny; 
+	public double maxx; 
+	public double maxy;
+	public int max_limit;
+	public Dimension size;
+	public Point start;
+	public Strategy strategy;
+	public Palette palette;
 
-    private double minx, miny, maxx, maxy;
-    private int limit;
-    private int xx, yy;
-    private BufferedImage img;
-    private parallel p;
-    private strategy s;
-    private palette pp;
+	public final Result 
+	call ()
+	{
+		BufferedImage image = new BufferedImage(size.width, size.height,
+		     BufferedImage.TYPE_INT_RGB);
+		Result result = new Result(start, image);
 
-    worker() {
-        img = new BufferedImage(60, 40, BufferedImage.TYPE_INT_RGB);
-    }
+		for (int y = 0; y < size.height; y++) 
+		{
+			final double ny = Interpolation.single(miny, maxy, 
+			    (double)y/(double)size.height);
 
-    /** Set parallel fractal renderer
-     * 
-     * @param _p parallel fractal renderer, used for get_new_job and put_part_img
-     */
-    public void set_parallel(parallel _p) {
-        System.out.print("worker::set_parallel");
-        p = _p;
-    }
+			for (int x = 0; x < size.width; x++) 
+			{
+				final double nx = Interpolation.single(minx, maxx, 
+				    (double)x/(double)size.width);
 
-    /** return generated image
-     * 
-     * @return generated image
-     */
-    public BufferedImage getImage() {
-        return img;
-    }
+				Complex c = new Complex(nx, -ny);
+				Complex z = new Complex(0.0, 0.0);
 
-    /** Method which generates specified part of fractal image. After the part is finished, thread does not exit, just asks for new job.
-     * 
-     */
-    @Override
-    public void run() {
-        while (true) {
-            info inf = p.get_new_job();
-            if (inf.done == true) {
-                break;
-            }
+				boolean escaped = false;
+				int limit;
+				for (limit = 0; limit < max_limit; limit++) 
+				{
+					z  = z.times(z).plus(c);
 
-            minx = inf.minx;
-            miny = inf.miny;
-            maxx = inf.maxx;
-            maxy = inf.maxy;
-            limit = inf.limit;
-            xx = inf.xx;
-            yy = inf.yy;
-            s = inf.s;
-            pp = inf.p;
+					final double r = z.re();
+					final double i = z.im();
+					if (i * i + r * r > 4.0) 
+					{
+						escaped = true;
+						break;
+					}
+				}
 
-            boolean morethan2 = false;
-            double xxx, yyx;
-            double r, i;
-            int pocet;
+				final double strategy_decision = strategy.decie(xx, yy, escaped,
+				    max_limit, limit);
+				img.setRGB(x, y, palette.decide(strategy_decision));
+			}
+		}
 
-            for (int y = 0; y < 40; y++) {
-                yyx = -((double) y / 40.0 * (maxy - miny) + miny);
-                for (int x = 0; x < 60; x++) {
-                    xxx = (double) x / 60.0 * (maxx - minx) + minx;
-                    Complex c = new Complex(xxx, yyx);
-                    Complex z = new Complex(0.0f, 0.0f);
-                    for (pocet = 0; pocet < limit; pocet++) {
-                        Complex momo = z.times(z);
-                        z = momo;
-
-                        momo = z.plus(c);
-                        z = momo;
-
-                        r = z.re();
-                        i = z.im();
-                        if (i * i + r * r > 4) {
-                            morethan2 = true;
-                            break;
-                        }
-                    }
-                    img.setRGB(x, y, pp.decide(s.decide(xx, yy, morethan2, limit, pocet)));
-                    morethan2 = false;
-                }
-            }
-            p.put_img_part(img, this.xx, this.yy);
-        }
-    }
+		return result;
+	}
 }
+
