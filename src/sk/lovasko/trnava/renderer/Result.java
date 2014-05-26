fd414f1a3626a4e682817e1028bd89ac427889f0
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-26 15:08:00 +0200

    Result class

---
+++ src/sk/lovasko/trnava/renderer/Result.java
@@ -0,0 +1,17 @@
+package sk.lovasko.trnava.renderer;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+public final class Result
+{
+	public BufferedImage image;
+	public Point start;
+
+	public Result (final BufferedImage image, final Point point)
+	{
+		this.image = image;
+		this.start = start;
+	}
+}
+
